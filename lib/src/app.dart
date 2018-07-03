5ed72eafefa99745deb77bc527064114ed1b3543
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-07-03 16:32:26 +0800

    90

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,26 +1,31 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import './models/image_model.dart';
 
 class App extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return AppState();
   }
 }
 
 class AppState extends State<App> {
-  int counter = 0;
+  int counter = 1;
+
+  void fetchImage() async {
+    counter++;
+    var res = await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    print(res);
+    var imageModel = ImageModel.fromJson(json.decode(res.body));
+  }
 
   Widget build(context) {
     return MaterialApp(
         home: Scaffold(
       body: Text('$counter'),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            counter += 1;
-          });
-        },
+        onPressed: fetchImage,
         child: Icon(Icons.clear),
       ),
       appBar: AppBar(
