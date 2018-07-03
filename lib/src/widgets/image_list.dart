cb6b60d17cd951ff3dfa05ba9c6567d4fd891c64
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-07-03 18:04:07 +0800

    102. padding bottom only

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -10,12 +10,24 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (BuildContext context, int index) {
-        return Container(
-            padding: EdgeInsets.all(20.0),
-            margin: EdgeInsets.all(20.0),
-            child: Image.network(images[index].url),
-            decoration: BoxDecoration(border: Border.all(color: Colors.grey)));
+        return buildImage(images[index]);
       },
     );
   }
+}
+
+Widget buildImage(ImageModel image) {
+  return Container(
+      padding: EdgeInsets.all(20.0),
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(bottom: 10.0),
+            child: Image.network(image.url),
+          ),
+          Text(image.title),
+        ],
+      ),
+      decoration: BoxDecoration(border: Border.all(color: Colors.grey)));
 }
